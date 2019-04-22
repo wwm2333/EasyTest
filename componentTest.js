83385090f78ca9d6e7fbba6d2cd7d9faaec2c276
wwm2333 <229483126@qq.com>
2019-04-22 10:58:46 +0800

    shadowDom and lifecycle

--- componentTest.js
+++ componentTest.js
@@ -9,6 +9,58 @@ class MyElement extends HTMLElement{
     constructor(){
         super();
         console.log('1');
+
+        //创建一个容器根节点：创建的时候自动添加到指定的对象上
+        //作用：作为容器；产生一个独立的私有作用域（组件作用域）
+       this._shadow= this.attachShadow({ //影子dom
+            mode:'open'
+        });
+        const span=document.createElement('span');
+        span.classList.add('color');
+        //数据的获取 getAttribute
+        const contents=JSON.parse(this.getAttribute("contents"))||['sdwsdasghadhgoiwngvks'];
+        span.innerHTML=contents[0];
+
+        //标签中嵌套的子元素
+        const div=document.createElement('div');
+        //div.innerHTML='<slot></slot>';//全部放到一起
+        div.innerHTML="<slot name='f'></slot>";//指定插槽名，html使用时加slot属性
+
+        const style=document.createElement('style');
+        style.textContent=`
+        .color{
+            color:red;
+
+        }`;
+        this._shadow.appendChild(span);
+        this._shadow.appendChild(style);
+        this._shadow.appendChild(div);
+
+        //组件内部的dom要通过shadow来取
+        //console.log(document.querySelector('span'));//获取不到
+        console.log(this._shadow.querySelector('span'));//可以取到
+   
+    }
+    //指定要监听的属性--attributeChangedCallback
+    static get observedAttributes(){
+        return ['contents','a'];
+    }
+     //生命周期
+     connectedCallback(){
+         console.log("connectedCallback");
+     }
+     disconnectedCallback(){
+        console.log("disconnectedCallback");
+    }
+    adoptedCallback(){
+        console.log("adoptedCallback");
+    }
+    attributeChangedCallback(contents,oldcontent,newcontent){
+        console.log("attributeChangedCallback",contents,oldcontent,newcontent);
+        if(contents=='contents'){
+            this._shadow.querySelector('span').innerHTML=newcontent;
+        }
+        
     }
 }
 
